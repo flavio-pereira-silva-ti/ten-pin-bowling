3f19846721cd1098147c6627b94048fc142d7778
Flavio Silva <fsilva1@grubhub.com>
2020-05-16 14:37:52 -0300

    init

---
+++ src/main/java/com/jobsity/tenpinbowling/TenPinBowlingApplication.java
@@ -0,0 +1,13 @@
+package com.jobsity.tenpinbowling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenPinBowlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TenPinBowlingApplication.class, args);
+    }
+
+}
